2c314c157d9f3073c7026fc00c3aba96dd8a54ad
Klaus <reverty@gmail.com>
2015-03-13 14:19:03 +0300

    Возврат к 1.0

--- classes/actions/ActionAjax.class.php
+++ classes/actions/ActionAjax.class.php
@@ -24,61 +24,29 @@ class PluginQa_ActionAjax extends PluginQa_Inherits_ActionAjax {
 	protected function EventSetBestComment() {
 
 		if(!$this->oUserCurrent){
-			E::ModuleMessage()->AddErrorSingle($this->Lang_Get('need_authorization'), $this->Lang_Get('error'));
+			 $this->Message_AddErrorSingle($this->Lang_Get('need_authorization'), $this->Lang_Get('error'));
 			return;
 		}
 
 		if (func_check(getRequest('commentId',null,'post'),'id',1,11))$commentId=getRequest('commentId',null,'post');
 		
 		if (!($oComment=$this->Comment_GetCommentById($commentId))) {
-			E::ModuleMessage()->AddErrorSingle($this->Lang_Get('system_error'), $this->Lang_Get('error'));
+			 $this->Message_AddErrorSingle($this->Lang_Get('system_error'), $this->Lang_Get('error'));
 			return;
 		} 
 		if($oComment->getTargetType()=='topic' && in_array($oComment->getTarget()->getType(), Config::Get('plugin.qa.allow_topic_type')) ){
 		
-			$oTopic = E::ModuleTopic()->GetTopicById($oComment->getTargetId());
+			$oTopic = $this->Topic_GetTopicById($oComment->getTargetId());
 			
 			$oTopic->setBestCommentId($oComment->getCommentId());
 			
-			E::ModuleTopic()->UpdateTopic($oTopic);
+			$this->Topic_UpdateTopic($oTopic);
 			
-			E::ModuleMessage()->AddNoticeSingle('Комментарий назначен правильным ответом', E::ModuleLang()->Get('attention'));
+			$this->Message_AddNoticeSingle('Комментарий назначен правильным ответом', E::ModuleLang()->Get('attention'));
 		}
 		
 	}
 	
-	protected function EventWhoVoteQuestion() {
-		if(!$this->oUserCurrent){
-			$this->Message_AddErrorSingle($this->Lang_Get('need_authorization'),$this->Lang_Get('error'));
-			return;
-		}
-		if(!Config::Get('plugin.voter.show_question_votes')) {
-			$this->Message_AddErrorSingle($this->Lang_Get('system_error'),$this->Lang_Get('error'));
-			return;
-		}
-		$this->Viewer_SetResponseAjax('json');
-		$id=0;
-		$num=0;
-		if (func_check(getRequest('id',null,'post'),'id',1,11))$id=getRequest('id',null,'post');
-		if (func_check(getRequest('num',null,'post'),'id',1,3))$num=getRequest('num',null,'post');
-		
-		if (!($oTopic=$this->Topic_GetTopicById($id))) {
-			$this->Message_AddErrorSingle($this->Lang_Get('system_error'),$this->Lang_Get('error'));
-			return;
-		}		
-		
-		$aVotes=$this->PluginVoterquestion_Topic_GetUserVotesQuestion($id,$num);
-		$this->Viewer_AssignAjax('count_votes',count($aVotes));
-		$this->Viewer_AssignAjax('votes',$aVotes);
-		
-		$strange_array = array();
-		$strange_array = $this->Lang_Get('plugin');
-		
-		$this->Viewer_AssignAjax('vote_for_this',$strange_array['voter']['vote_for_this'] );
-		
-	}
-	
-
 }
 
 ?>
